ae48c16fc1a5b8a25e54c2f9f894621f0927a210
Georgii Surkov <georgii.surkov@outlook.com>
2022-01-29 15:28:55 +0300

    Add number prefixes in tests

--- test_printf.c
+++ test_printf.c
@@ -16,9 +16,9 @@ int main(void)
 {
     TEST_PRINTF("%s %s", "Hello there!", "General Kenobi!")
     TEST_PRINTF("Some integers: %+.30d, %30d, %.5d", 42, -346650169, -69);
-    TEST_PRINTF("Some hex integers: %x, %X", 0xdeadbeef, 0xbadface);
-    TEST_PRINTF("Some negative hex integers: %x, %.12X", -1, -0xdead);
-    TEST_PRINTF("Some octal integers: %o, %o", 8, -27);
+    TEST_PRINTF("Some hex integers: 0x%x, 0x%X", 0xdeadbeef, 0xbadface);
+    TEST_PRINTF("Some negative hex integers: 0x%x, 0x%.12X", -1, -0xdead);
+    TEST_PRINTF("Some octal integers: 0%o, 0%o", 8, -27);
     TEST_PRINTF("Some floats: %+.5f, %9.4f, %f", 3.14159f, -23.8f, -0.0f);
 
     return 0;
